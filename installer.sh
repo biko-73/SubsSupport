1e533986f66c4819bf85fbf1a190f02067b6fd8b
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-09-15 02:17:49 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -13,8 +13,8 @@
 # Plugin	... Enter Manually
 ##############################################################################################################
 
-MY_IPK_PY2="enigma2-plugin-extensions-subssupport-py2_1.5.8_all.ipk"
-MY_IPK_PY3="enigma2-plugin-extensions-subssupport_1.5.8-r7-py3_all.ipk"
+MY_IPK_PY2="enigma2-plugin-extensions-subssupport_1.5.8-r9-py2_all.ipk"
+MY_IPK_PY3="enigma2-plugin-extensions-subssupport_1.5.8-r9-py3_all_all.ipk"
 
 MY_DEB_PY2="enigma2-plugin-extensions-subssupport_1.5.8-r2_all.deb"
 MY_DEB_PY3=""
